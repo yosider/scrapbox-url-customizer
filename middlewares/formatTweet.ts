cbbfb36e992611a317ad0b1593420427d8d7e3e0
yosider <yosider.on.earth@gmail.com>
2024-01-19 02:04:10 +0900

    modify tweet format

--- middlewares/formatTweet.ts
+++ middlewares/formatTweet.ts
@@ -46,14 +46,14 @@ const defaultFormat = (tweet: Tweet | RefTweet | TweetInfo, url: URL) => {
   return [
     ...(replyTo
       ? [
-        ...stringify(replyTo).map((line) => ` > ${line}`),
         ...(replyTo.quote
-          ? stringify(replyTo.quote).map((line) => `  > ${line}`)
+          ? stringify(replyTo.quote).map((line) => `> ${line}`)
           : []),
+        ...stringify(replyTo).map((line) => `> ${line}`),
       ]
       : []),
     ...stringify(processed).map((line) => `> ${line}`),
-    ...(quote ? stringify(quote).map((line) => `> > ${line}`) : []),
+    ...(quote ? stringify(quote).map((line) => ` > ${line}`) : []),
   ].join("\n");
 };
 
